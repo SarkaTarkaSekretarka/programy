ca04d8a26ec387f175e8a0f98991cb7e94512e28
SarkaTarkaSekretarka <sara.maruszczyk@vip.onet.pl>
2019-11-10 15:26:15 +0100

    init project

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <cstdlib>
+#include "time.h"
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
